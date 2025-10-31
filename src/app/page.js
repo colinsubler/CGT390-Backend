7fe1a93a22af964a68b864eda76462e9a6794590
colinsubler <csubler@purdue.edu>
2025-10-31 11:04:53 -0400

    fixed 2 issues

--- src/app/page.js
+++ src/app/page.js
@@ -3,25 +3,29 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 async function getTitles() {
-  const response = await fetch("https://web.ics.purdue.edu/~zong6/profile-app/get-titles.php",
+  const response = await fetch(
+    "https://web.ics.purdue.edu/~zong6/profile-app/get-titles.php",
     { next: { revalidate: 60 } }
-  )
+  );
   const data = await response.json();
-  return data?data.titles:[];
+  return data ? data.titles : [];
 }
 
 async function getProfiles(title, search) {
-  const response = await fetch("https://web.ics.purdue.edu/~zong6/profile-app/fetch-data-with-filter.php?title=${title}&search=${search}&limit=1000",
+  const response = await fetch(
+    `https://web.ics.purdue.edu/~zong6/profile-app/fetch-data-with-filter.php?title=${encodeURIComponent(title || "")}&search=${encodeURIComponent(search || "")}&limit=1000`,
     { next: { revalidate: 60 } }
-  )
+  );
   const data = await response.json();
-  return data?data.profiles:[];
+  return data ? data.profiles : [];
 }
 
-export default async function HomePage({searchParams}) {
-
-  const {title, search} = await searchParams;
-  const [titles, profiles] = await Promise.all([getTitles(), getProfiles(title, search)]);
+export default async function HomePage({ searchParams }) {
+  const { title = "", search = "" } = searchParams || {};
+  const [titles, profiles] = await Promise.all([
+    getTitles(),
+    getProfiles(title, search),
+  ]);
 
   return (
     <div className={styles.container}>
@@ -29,7 +33,7 @@ export default async function HomePage({searchParams}) {
       <main className={styles.main}>
         <h1>Profiles</h1>
         <form action="/" method="get">
-          <select name="title" defaultValue={title || ""} className={styles.select}>
+          <select name="title" defaultValue={title} className={styles.select}>
             <option value="">All Titles</option>
             {titles.map((t) => (
               <option key={t} value={t}>
@@ -40,7 +44,7 @@ export default async function HomePage({searchParams}) {
           <input
             type="text"
             name="search"
-            defaultValue={search || ""}
+            defaultValue={search}
             placeholder="Search by name"
             className={styles.input}
           />
